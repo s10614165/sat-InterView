dc3363bd77a39412cfbed9653ef2c4fcf4e530cb
s10614165 <kweifon1998@gmail.com>
2023-04-16 00:40:31 +0800

    feat Progressbar layout

--- src/component/Progressbar.jsx
+++ src/component/Progressbar.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 
 const Progressbar = () => {
+  const percent =80
   return (
-    <div className='mt-4'>i'm Progressbar</div>
+    <div className='mt-4'>
+    <div className='flex flex-row justify-between'>
+    <div > {`${percent}%`}</div>
+   <div className='w-[90%] flex items-center'>
+   <div className="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+     <div className={`bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full`}
+     style={{width: percent +'%'}}>
+     </div>
+     </div>
+   </div>
+    </div>
+   
+
+    </div>
   )
 }
 
